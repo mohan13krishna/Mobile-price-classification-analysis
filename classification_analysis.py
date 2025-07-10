67f922fd475bf78bbef69ded6a42a8ba03818533
Mohan Krishna Thalla <142295454+mohan13krishna@users.noreply.github.com>
2025-07-10 18:20:46 +0530

    Update classification_analysis.py

--- classification_analysis.py
+++ classification_analysis.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns           
      
 from sklearn.model_selection import train_test_split 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler 
 from sklearn.linear_model import LogisticRegression 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
